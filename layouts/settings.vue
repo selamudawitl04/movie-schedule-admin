8fe3120aaf55212cafbe307ba69d8aa2c8cf854e
selamudawit <selamudev@gmail.com>
2024-01-18 00:10:40 -0500

    FEAT add pricings feature + add query + add ui + integration

---
+++ layouts/settings.vue
@@ -0,0 +1,26 @@
+<script></script>
+
+<template>
+  <NuxtLayout name="adminpanel">
+    <div class="flex">
+      <!-- ----------------Message type links----------- -->
+      <div
+        class="flex flex-col space-y-8 text-xl py-10 secondary-text font-medium w-[15%]"
+      >
+        <nuxt-link active-class="text-primary-500" to="/app/pricing"
+          >Pricings</nuxt-link
+        >
+        <nuxt-link active-class="text-primary-500" to="/app/service_requests"
+          >Service Requests</nuxt-link
+        >
+        <nuxt-link active-class="text-primary-500" to="/app/contact_us"
+          >Contact Us</nuxt-link
+        >
+      </div>
+
+      <div class="w-[85%] pl-10 border-l">
+        <slot></slot>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
---
+++ pages/pricings/add.vue
@@ -0,0 +1,29 @@
+<script setup>
+onResult((result) => {
+  pricings.value = result.data.pricings;
+});
+
+definePageMetadata({
+  layout: "settings",
+});
+</script>
+<template>
+  <section class="bg-white mb-44 py-32">
+    <h2 class="text-3xl font-bold text-primary9 py-8 text-center">
+      Our Pricing Plan
+    </h2>
+    <div
+      class="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8"
+    >
+      <div
+        v-for="pricing in pricings"
+        :key="pricing.id"
+        class="bg-white shadow rounded-lg p-6 w-72"
+      >
+        <h2 class="text-xl font-medium mb-4">{{ pricing.title }}</h2>
+        <p class="text-gray-600 mb-4">{{ pricing.description }}</p>
+        <p class="text-3xl font-bold mb-4">{{ pricing.price }} Birr</p>
+      </div>
+    </div>
+  </section>
+</template>
